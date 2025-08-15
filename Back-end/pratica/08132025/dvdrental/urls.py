b4eb21bf5b37a889fe7dde688e414f57b855088c
Gabriel Evaristo <gabriel.evaristo.2004@gmail.com>
2025-08-14 22:09:38 -0300

    backend(14082025): pratica com consultas

--- Back-end/pratica/08132025/dvdrental/urls.py
+++ Back-end/pratica/08132025/dvdrental/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('customers/', views.customers, name='customers'),
-    path('cities/', views.cities, name='cities')
+    path('customers/details/<int:id>', views.details, name='details'),
+    path('cities/', views.cities, name='cities'),
+    path('countries', views.countries, name='countries'),
+    path('countries/<int:id>', views.country_cities, name='country_cities')
 ]
--- Back-end/pratica/08132025/dvdrental/views.py
+++ Back-end/pratica/08132025/dvdrental/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404
 
-from dvdrental.models import Customer, City
+from dvdrental.models import Customer, City, Rental, Country
 
 def customers(request):
     customers = Customer.objects.all().values()
@@ -17,4 +18,38 @@ def cities(request):
     context = {
         'cities': cities
     }
+    return HttpResponse(template.render(context=context, request=request))
+
+def details(request, id):
+    details = Rental.objects.filter(customer_id=id)
+    customer = get_object_or_404(Customer, pk=id)
+
+    template = loader.get_template('customer_details.html')
+    context = {
+        'details': details,
+        'customer_name': f"{customer.first_name} {customer.last_name}"
+    }
+
+    return HttpResponse(template.render(context=context, request=request))
+
+def countries(request):
+    countries = Country.objects.all().values()
+
+    template = loader.get_template('all_countries.html')
+    context = {
+        'countries': countries
+    }
+
+    return HttpResponse(template.render(context=context, request=request))
+
+def country_cities(request, id):
+    country = get_object_or_404(Country, pk=id)
+    cities = City.objects.filter(country_id=id)
+
+    template = loader.get_template('country_cities.html')
+    context = {
+        'country': country,
+        'cities': cities
+    }
+
     return HttpResponse(template.render(context=context, request=request))
